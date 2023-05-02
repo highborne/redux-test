1687683f0cce73185f2378558e6611e8200a7f28
Everton Cardoso <evertonluizcoster@gmail.com>
2023-05-02 19:41:19 -0300

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'Highborne template',
+  description: 'Model repository for new projects',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
